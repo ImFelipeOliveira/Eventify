1826f9f2fdad20bd41f11fb51c44e410700f0992
Felipe Oliveira <flipeaz342@gmail.com>
2025-02-16 14:30:40 -0400

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,38 @@
+import { Provider } from "@/components/ui/provider";
+
+import Head from "next/head";
+import Link from "next/link";
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import favicon from "./static/favicon.ico";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Eventify",
+  description: "",
+  icons: favicon.src,
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Provider>{children}</Provider>
+      </body>
+    </html>
+  );
+}
